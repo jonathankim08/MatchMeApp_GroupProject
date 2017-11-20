e1fbe8d3fdd125f81b9e7ce1d1657563bbc623c4
arjunkottana <arjunkot@umich.edu>
2017-11-20 11:33:19 -0500

    arjun test

--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/MainActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/MainActivity.java
@@ -5,18 +5,14 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
-    /* jyoty checking
-
-     */
+    /* jyoty checking     */
     // Jonathan test
+    //arjun is able to collaborate
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //arjun is able to collaborate
-
 
     }
 }
