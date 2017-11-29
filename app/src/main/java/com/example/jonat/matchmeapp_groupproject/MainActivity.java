756cfc65cb4b1af8a1bef66f97ffe830f874102f
arjunkottana <arjunkot@umich.edu>
2017-11-29 13:39:37 -0500

    arjun test 2

--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/MainActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends Activity {
     // Jonathan test
     //arjun is able to collaborate
     //Mike is able to collaborate!!!
+    // arjun test 2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
