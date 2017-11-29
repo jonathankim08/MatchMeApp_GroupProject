73357a250f816b2c37fc1c28601ee249acf6fd62
neilsk <neilsk@umich.edu>
2017-11-29 13:47:21 -0500

    Neil Test

--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/MainActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/MainActivity.java
@@ -5,19 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
-    //MAINACTIVITY WILL BE THE LOGIN PAGE
-    /* jyoty checking     */
-    // Jonathan test
-    //arjun is able to collaborate
-    //Mike is able to collaborate!!!
-    // arjun test 2
-
-    // Jonathan test 2
-
-
-
-    // Neil is here !!
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
