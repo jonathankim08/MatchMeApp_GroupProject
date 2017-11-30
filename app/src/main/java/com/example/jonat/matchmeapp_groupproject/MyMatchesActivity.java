26e59605fdd2a6ea5dc23002f3479451d3155357
mevertsberg <meverts@umich.edu>
2017-11-30 13:26:53 -0500

    Mike test

--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/MyMatchesActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/MyMatchesActivity.java
@@ -2,12 +2,21 @@ package com.example.jonat.matchmeapp_groupproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MyMatchesActivity extends Activity {
+public class MyMatchesActivity extends Activity implements View.OnClickListener{
+
+    private EditText etTitle, etMyatches;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_matches);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
