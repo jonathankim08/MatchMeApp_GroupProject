34986f697b04670ab92f5c31f1bd655cfc644422
mevertsberg <meverts@umich.edu>
2017-11-30 13:49:39 -0500

    Mike test

--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/MyMatchesActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/MyMatchesActivity.java
@@ -3,16 +3,26 @@ package com.example.jonat.matchmeapp_groupproject;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class MyMatchesActivity extends Activity implements View.OnClickListener{
 
     private EditText etTitle, etMyatches;
 
+    String[] mobileArray = {"User 1","User 2","User 3","Uber 4", "User 5"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_matches);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_my_matches, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.user_list);
+        listView.setAdapter(adapter);
     }
 
     @Override
