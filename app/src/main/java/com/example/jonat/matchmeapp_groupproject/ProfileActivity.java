613ee6878b69b59a9374e9ca304d4efd86cdb8fe
jonathankim08 <jonathankim08@gmail.com>
2017-12-01 17:24:16 -0500

    Jonathan Test 2

--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/ProfileActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/ProfileActivity.java
@@ -2,12 +2,41 @@ package com.example.jonat.matchmeapp_groupproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class ProfileActivity extends Activity {
+public class ProfileActivity extends Activity implements View.OnClickListener {
+
+    //declare objects
+    private TextView textViewEditProfile, textViewLocation, textViewTennisLevel, textViewChessLevel;
+    private EditText editTextLocation;
+    private Spinner spinnerTennisLevel, spinnerChessLevel;
+    private Button buttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //link objects
+        textViewEditProfile = (TextView) findViewById(R.id.textViewEditProfile);
+        textViewLocation = (TextView) findViewById(R.id.textViewLocation);
+        textViewTennisLevel = (TextView) findViewById(R.id.textViewTennisLevel);
+        textViewChessLevel = (TextView) findViewById(R.id.textViewChessLevel);
+        editTextLocation = (EditText) findViewById(R.id.editTextLocation);
+        spinnerTennisLevel = (Spinner) findViewById(R.id.spinnerTennisLevel);
+        spinnerChessLevel = (Spinner) findViewById(R.id.spinnerChessLevel);
+        buttonSubmit = (Button) findViewById(R.id.buttonSubmit);
+
+        //set up button listener
+        buttonSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/RegistrationActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/RegistrationActivity.java
@@ -2,12 +2,46 @@ package com.example.jonat.matchmeapp_groupproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class RegistrationActivity extends Activity {
+import org.w3c.dom.Text;
+
+public class RegistrationActivity extends Activity implements View.OnClickListener {
+
+    //declare objects
+    private TextView textViewEditProfile, textViewName, textViewLocation, textViewTennisLevel, textViewChessLevel;
+    private EditText editTextName,editTextLocation;
+    private Spinner spinnerTennisLevel, spinnerChessLevel;
+    private Button buttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        //link objects
+        textViewEditProfile = (TextView) findViewById(R.id.textViewEditProfile);
+        textViewName = (TextView) findViewById(R.id.textViewName);
+        textViewLocation = (TextView) findViewById(R.id.textViewLocation);
+        textViewTennisLevel = (TextView) findViewById(R.id.textViewTennisLevel);
+        textViewChessLevel = (TextView) findViewById(R.id.textViewChessLevel);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextLocation = (EditText) findViewById(R.id.editTextLocation);
+        spinnerTennisLevel = (Spinner) findViewById(R.id.spinnerTennisLevel);
+        spinnerChessLevel = (Spinner) findViewById(R.id.spinnerChessLevel);
+        buttonSubmit = (Button) findViewById(R.id.buttonSubmit);
+
+        //set up button listener
+        buttonSubmit.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
