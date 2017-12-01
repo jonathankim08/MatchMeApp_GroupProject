93ec981ecc426dc7ee3998bfd992d5df030c6a13
mevertsberg <meverts@umich.edu>
2017-12-01 11:27:51 -0600

    Mike My Matches Update

--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/MyMatchesActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/MyMatchesActivity.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class MyMatchesActivity extends Activity implements View.OnClickListener{
 
     private EditText etTitle, etMyatches;
-    private ListView lvCurrentMatches;
+    private ListView lvCurrentMatches, lvPastMatches;
 
     private String[] NAMES  = {"Mike","Neil","Jyoty","Jonathan","Arjun"};
     private String[] GAMES = {"Tennis","Chess"};
@@ -28,28 +28,26 @@ public class MyMatchesActivity extends Activity implements View.OnClickListener{
         setContentView(R.layout.activity_my_matches);
 
         lvCurrentMatches = findViewById(R.id.lvCurrentMatches);
+        lvPastMatches = findViewById(R.id.lvPastMatches);
 
         CustomAdaptor customAdaptor = new CustomAdaptor();
         lvCurrentMatches.setAdapter(customAdaptor);
+        lvPastMatches.setAdapter(customAdaptor);
     }
 
     class CustomAdaptor extends BaseAdapter {
-
         @Override
         public int getCount() {
             return NAMES.length;
         }
-
         @Override
         public Object getItem(int i) {
             return null;
         }
-
         @Override
         public long getItemId(int i) {
             return 0;
         }
-
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
 
@@ -68,6 +66,7 @@ public class MyMatchesActivity extends Activity implements View.OnClickListener{
         }
     }
 
+    
     @Override
     public void onClick(View view) {
 
