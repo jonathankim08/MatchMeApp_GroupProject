5574b1f975be510090b7da8f02712267cd678612
jonathankim08 <jonathankim08@gmail.com>
2017-12-01 16:16:14 -0500

    Jonathan Test 2

--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/ChatActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/ChatActivity.java
@@ -2,12 +2,83 @@ package com.example.jonat.matchmeapp_groupproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class ChatActivity extends Activity {
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class ChatActivity extends Activity implements View.OnClickListener {
+
+    private EditText editTextChat;
+    private Button buttonSubmit;
+    private ListView listViewMessages;
+
+    ArrayList<String> list = new ArrayList<>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        editTextChat = findViewById(R.id.editTextChat);
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+        listViewMessages = findViewById(R.id.listViewMessages);
+
+        buttonSubmit.setOnClickListener(this);
+
+        adapter = new ArrayAdapter<String>(this, R.layout.chatlayout, R.id.textViewChat, list);
+        listViewMessages.setAdapter(adapter);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("chat");
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String chat;
+                chat = dataSnapshot.getValue(String.class);
+                list.add(chat);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("chat");
+
+        myRef.push().setValue(editTextChat.getText().toString());
     }
 }
