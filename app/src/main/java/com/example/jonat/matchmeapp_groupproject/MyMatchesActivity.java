b1bf68f1dc9c70715a56b0b7ae5ce4a8fe659a54
mevertsberg <meverts@umich.edu>
2017-12-01 10:47:16 -0600

    Mike My Matches Update

--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/MyMatchesActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/MyMatchesActivity.java
@@ -3,26 +3,55 @@ package com.example.jonat.matchmeapp_groupproject;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.List;
 
 public class MyMatchesActivity extends Activity implements View.OnClickListener{
 
     private EditText etTitle, etMyatches;
+    private ListView lvCurrentMatches;
 
-    String[] mobileArray = {"User 1","User 2","User 3","Uber 4", "User 5"};
+    private String[] NAMES  = {"Mike","Neil","Jyoty","Jonathan","Arjun"};
+    private String[] GAMES = {"Tennis","Chess"};
+    private String[] DATEANDTIME = {"Monday, 9-11am","Friday 6-8pm","Saturday 1-3pm"};
+    private String[] LOCATION = {"Burns Park Tennis Courts","Union Chess Hall"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_matches);
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,
-                R.layout.activity_my_matches, mobileArray);
+        lvCurrentMatches = findViewById(R.id.lvCurrentMatches);
+    }
 
-        ListView listView = (ListView) findViewById(R.id.user_list);
-        listView.setAdapter(adapter);
+    class CustomAdaptor extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return NAMES.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+
+            return null;
+        }
     }
 
     @Override
