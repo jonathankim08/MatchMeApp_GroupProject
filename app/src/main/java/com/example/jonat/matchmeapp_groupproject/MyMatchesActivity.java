b374148f00d7c90ca6f88c215a233ecdd4335b62
mevertsberg <meverts@umich.edu>
2017-12-01 20:29:05 -0600

    Mike My Matches Update

--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/MyMatchesActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/MyMatchesActivity.java
@@ -18,25 +18,36 @@ import java.util.List;
 
 public class MyMatchesActivity extends Activity implements View.OnClickListener{
 
-    private EditText etTitle, etMyatches;
+    private EditText etTitle, etMyatches, etPastMatches;
     private ListView lvCurrentMatches, lvPastMatches;
 
-    private String[] NAMES  = {"Mike","Neil"};
+    private String[] NAMES  = {"Jyoty","Neil"};
     private String[] GAMES = {"Tennis","Chess"};
     private String[] DAYTIME = {"Friday 6:00-8:00 PM","Saturday 1:00-3:00 PM"};
     private String[] LOCATION = {"Burns Park Tennis Courts","Union Chess Hall"};
+    private int[] IMAGES = {R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_matches);
 
+        etTitle = findViewById(R.id.etTitle);
+        etMyatches = findViewById(R.id.etMyMatches);
+        etPastMatches = findViewById(R.id.etPastMatches);
         lvCurrentMatches = findViewById(R.id.lvCurrentMatches);
         lvPastMatches = findViewById(R.id.lvPastMatches);
 
         CustomAdaptor customAdaptor = new CustomAdaptor();
         lvCurrentMatches.setAdapter(customAdaptor);
         lvPastMatches.setAdapter(customAdaptor);
+
+        lvCurrentMatches.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
     }
 
     class CustomAdaptor extends BaseAdapter {
@@ -55,18 +66,20 @@ public class MyMatchesActivity extends Activity implements View.OnClickListener{
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
 
-            view = getLayoutInflater().inflate(R.layout.currentmatcheslayout, null);
+            view = getLayoutInflater().inflate(R.layout.currentmatcheslayout,null);
             TextView tvName = view.findViewById(R.id.tvName);
             TextView tvGame = view.findViewById(R.id.tvGame);
             TextView tvDayTime = view.findViewById(R.id.tvDayTime);
             TextView tvLocation = view.findViewById(R.id.tvLocation);
+            ImageView ivPicture = view.findViewById(R.id.ivPicture);
 
             tvName.setText(NAMES[i]);
             tvGame.setText(GAMES[i]);
             tvDayTime.setText(DAYTIME[i]);
             tvLocation.setText(LOCATION[i]);
+            ivPicture.setImageResource(IMAGES[i]);
 
-            return null;
+            return view;
         }
     }
 
