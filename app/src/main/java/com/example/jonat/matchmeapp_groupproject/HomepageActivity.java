4924a950d2958fbde1c4c18fda7fd8853e62a9ca
jyo19 <jyoty@umich.edu>
2017-12-02 00:45:01 -0500

    Jyoty testing Git

--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/HomepageActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/HomepageActivity.java
@@ -1,13 +1,18 @@
 package com.example.jonat.matchmeapp_groupproject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class HomepageActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homepage);
+
     }
+
 }
