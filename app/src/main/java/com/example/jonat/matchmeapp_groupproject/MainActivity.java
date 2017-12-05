80b9d6c4ca2775bab6e246e8d617d7f7cddfd32b
arjunkottana <arjunkot@umich.edu>
2017-12-05 14:30:09 -0500

    menu code in all pages

--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/MainActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -94,6 +97,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
         }
 
     }
+
 
     private void createAccount(String email, String password) {
 
--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/MyMatchesActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/MyMatchesActivity.java
@@ -2,8 +2,12 @@ package com.example.jonat.matchmeapp_groupproject;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -97,5 +101,39 @@ public class MyMatchesActivity extends Activity implements View.OnClickListener{
     @Override
     public void onClick(View view) {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mainMenuInflater = getMenuInflater();
+        mainMenuInflater.inflate(R.menu.mainmenu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.homeMenu){
+            Intent intentHome = new Intent(this,HomepageActivity.class);
+            this.startActivity(intentHome);
+        }else if(item.getItemId() == R.id.myPotentialMatchesMenu){
+            Intent intentMyPotentialMatches = new Intent(this,MyPotentialMatchesActivity.class);
+            this.startActivity(intentMyPotentialMatches);
+        }else if(item.getItemId() == R.id.myMatchesMenu){
+            Intent intentMyMatches = new Intent(this,MyMatchesActivity.class);
+            this.startActivity(intentMyMatches);
+        }else if (item.getItemId() == R.id.chatMenu){
+            Intent intentChat = new Intent(this,ChatActivity.class);
+            this.startActivity(intentChat);
+        }else if (item.getItemId() == R.id.updateProfileMenu){
+            Intent intentUpdateProfile = new Intent(this,RegistrationActivity.class);
+            this.startActivity(intentUpdateProfile);
+        }else if (item.getItemId() == R.id.logoutMenu){
+            Intent intentLogout = new Intent(this,MainActivity.class);
+            this.startActivity(intentLogout);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
