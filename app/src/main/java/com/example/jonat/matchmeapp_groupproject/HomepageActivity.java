9c6d1c74cfc4ecd7677e5b42c98d0c85a804d8ec
jyo19 <jyoty@umich.edu>
2017-12-05 14:30:37 -0500

    Jyoty testing Git

--- app/src/main/java/com/example/jonat/matchmeapp_groupproject/HomepageActivity.java
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/HomepageActivity.java
@@ -4,14 +4,34 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RadioButton;
 
 public class HomepageActivity extends Activity {
+
+    public HomepageActivity() {
+    }
+
+    private Button buttonSlot1, buttonSlot2, buttonSlot3, buttonSlot4, buttonSlot5,buttonSlot6;
+    private RadioButton radiobuttonchess, radiobuttontennis;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        buttonSlot1 = findViewById(R.id.slot1);
+        buttonSlot2 = findViewById(R.id.slot2);
+        buttonSlot3 = findViewById(R.id.slot3);
+        buttonSlot4 = findViewById(R.id.slot4);
+        buttonSlot5 = findViewById(R.id.slot5);
+        buttonSlot6 = findViewById(R.id.slot6);
+        radiobuttonchess = findViewById(R.id.chessRadButton);
+        radiobuttontennis = findViewById(R.id.tennisRadButton);
+
+
+
 
     }
 
