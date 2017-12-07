f7f20c802d634146911d8778802ee2074b5687bc
mevertsberg <meverts@umich.edu>
2017-12-06 22:54:17 -0500

    profile edits

---
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/OtherUsersProfiles.java
@@ -0,0 +1,13 @@
+package com.example.jonat.matchmeapp_groupproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OtherUsersProfiles extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_users_profiles);
+    }
+}
