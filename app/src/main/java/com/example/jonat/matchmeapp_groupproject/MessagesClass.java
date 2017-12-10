9ee28ab0282530e1865eda83d4108ef6dddeebfa
jonathankim08 <jonathankim08@gmail.com>
2017-12-10 14:24:43 -0500

    Merge branch 'master' of C:\Apps\MatchMeApp_GroupProject with conflicts.

---
+++ app/src/main/java/com/example/jonat/matchmeapp_groupproject/MessagesClass.java
@@ -0,0 +1,9 @@
+package com.example.jonat.matchmeapp_groupproject;
+
+/**
+ * Created by jonat on 12/10/2017.
+ */
+
+public class MessagesClass {
+    public String messagesSender, messagesReceiver, messagesMessageText;
+}
